f7bddb6df85e496ad07dd6efcaa55cd93af7c228
Kenneth Evans <ken@kenevans.net>
2017-01-03 17:10:02 -0500

    Is working to get images from vCards from a hard-coded filename and save them to a hard-coded folder.

--- VCardExtractPhotos/Program.cs
+++ VCardExtractPhotos/Program.cs
@@ -1,12 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using EWSoftware.PDI.Objects;
+using EWSoftware.PDI.Parser;
+using EWSoftware.PDI.Properties;
 
 namespace VCardExtractPhotos {
     class Program {
+        public static string LF = System.Environment.NewLine;
+        private static VCardCollection vCards;
+        private static string inutFileName = @"C:\Scratch\Contacts\Contacts-S7-2016-12-29.vcf";
+        //private static string inutFileName = @"C:\Scratch\Contacts\Contacts-S7-2016-12-29.KennethEvans.vcf";
+        private static string outputDirName = @"C:\Scratch\Contacts\Photos\";
+
+        private static void saveImages() {
+            try {
+                vCards = VCardParser.ParseFromFile(inutFileName);
+            } catch (Exception ex) {
+                string error = String.Format("Unable to load vCards:\n{0}",
+                    ex.Message);
+                if (ex.InnerException != null) {
+                    error += ex.InnerException.Message + "\n";
+                    if (ex.InnerException.InnerException != null)
+                        error += ex.InnerException.InnerException.Message;
+                }
+                System.Diagnostics.Debug.Write(ex);
+                Utils.excMsg(error, ex);
+                return;
+            }
+
+            // Loop over cards
+            string imageFileName;
+            bool hasImage;
+            string imageType;
+            string info = "";
+            foreach (VCard vCard in vCards) {
+                hasImage = false;
+                imageFileName = null;
+                info += vCard.Name.FormattedName + ": ";
+                if (vCard.Photo.Value != null) {
+                    hasImage = true;
+                    imageType = vCard.Photo.ImageType;
+                    if (vCard.Photo.ValueLocation == ValLocValue.Binary) {
+                        info += "Binary Image: " + imageType;
+                    } else {
+                        imageFileName = vCard.Photo.Value;
+                        info += "Image: " + imageType + " " + imageFileName;
+                    }
+                } else {
+                    hasImage = false;
+                    info += "No Image";
+                }
+                info += LF;
+                Console.Write(info);
+                if (hasImage) {
+                    saveImage(vCard);
+                }
+            }
+        }
+
+        static void saveImage(VCard vCard) {
+            PhotoProperty photo = vCard.Photo;
+            if (photo.ValueLocation != ValLocValue.Binary) {
+                return;
+            }
+            byte[] imageBytes = photo.GetImageBytes();
+            Bitmap bm;
+            try {
+                bm = new Bitmap(new System.IO.MemoryStream(imageBytes));
+            } catch (Exception ex) {
+                Utils.excMsg("Failed to create Bitmap", ex);
+                return;
+            }
+            try {
+                string fileName = outputDirName + vCard.Name.FormattedName
+                    + ".png";
+                bm.Save(fileName, ImageFormat.Png);
+                Console.Write("  Saved photo as " + fileName + LF);
+            } catch (Exception ex) {
+                Utils.excMsg("Failed to save Bitmap", ex);
+            }
+        }
+
         static void Main(string[] args) {
+            saveImages();
         }
     }
 }
---
+++ VCardExtractPhotos/Utils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace VCardExtractPhotos {
+    public static class Utils {
+        public static string LF = System.Environment.NewLine;
+
+        /// <summary>
+        /// Error message.
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void errMsg(string msg) {
+
+            Console.WriteLine("Error: " + msg);
+        }
+
+        /// <summary>
+        /// Exception message.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="ex"></param>
+        public static void excMsg(string msg, Exception ex) {
+            Console.WriteLine("Error: " + msg + LF + "Exception: " + ex + LF
+            + ex.Message);
+        }
+
+        /// <summary>
+        /// Warning message.
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void warnMsg(string msg) {
+            Console.WriteLine("Warning: " + msg);
+        }
+
+        /// <summary>
+        /// Information message.
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void infoMsg(string msg) {
+            Console.WriteLine("Info: " + msg);
+        }
+
+    }
+}
