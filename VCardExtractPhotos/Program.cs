4da6045fdae750e0ad744bba6910e461151225ac
Kenneth Evans <ken@kenevans.net>
2017-01-03 19:40:37 -0500

    Added transferImages.

--- VCardExtractPhotos/Program.cs
+++ VCardExtractPhotos/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using EWSoftware.PDI.Objects;
 using EWSoftware.PDI.Parser;
 using EWSoftware.PDI.Properties;
@@ -8,17 +9,110 @@ using EWSoftware.PDI.Properties;
 namespace VCardExtractPhotos {
     class Program {
         public static string LF = System.Environment.NewLine;
-        private static VCardCollection vCards;
-        private static string inutFileName = @"C:\Scratch\Contacts\Contacts-S7-2016-12-29.vcf";
+        /// <summary>
+        /// The input file name for saveImages and the file with images for
+        /// transferImages.
+        /// </summary>
+        private static string inputFileName = @"C:\Scratch\Contacts\Contacts-S7-2016-12-29.vcf";
         //private static string inutFileName = @"C:\Scratch\Contacts\Contacts-S7-2016-12-29.KennethEvans.vcf";
+        /// <summary>
+        /// The file without images for transferImages.
+        /// </summary>
+        private static string inputFileName2 = @"C:\Scratch\Contacts\google.contacts.MyContacts.010217.vcf";
+        /// <summary>
+        /// The out directory for saveImages.  The image files go there.
+        /// </summary>
         private static string outputDirName = @"C:\Scratch\Contacts\Photos\";
+        /// <summary>
+        /// The output directory for the converted file for transferImages.  The
+        /// converted file goes there.
+        /// </summary>
+        private static string outputDirName2 = @"C:\Scratch\Contacts\";
 
-        private static void saveImages() {
+        /// <summary>
+        /// Transfers images in inputFileName to vCards in inputFileName2
+        /// having the same FormattedName and saves the result in outputDirName2.
+        /// </summary>
+        private static void transferImages() {
+            VCardCollection vCards, vCards2;
             try {
-                vCards = VCardParser.ParseFromFile(inutFileName);
+                vCards = VCardParser.ParseFromFile(inputFileName);
             } catch (Exception ex) {
-                string error = String.Format("Unable to load vCards:\n{0}",
+                string error = String.Format("Unable to load vCards from "
+                    + inputFileName + ":\n{0}",
                     ex.Message);
+                if (ex.InnerException != null) {
+                    error += ex.InnerException.Message + "\n";
+                    if (ex.InnerException.InnerException != null)
+                        error += ex.InnerException.InnerException.Message;
+                }
+                System.Diagnostics.Debug.Write(ex);
+                Utils.excMsg(error, ex);
+                return;
+            }
+            Console.WriteLine("Read " + inputFileName);
+            Console.WriteLine("There are " + vCards.Count + " vCards");
+            try {
+                vCards2 = VCardParser.ParseFromFile(inputFileName2);
+            } catch (Exception ex) {
+                string error = String.Format("Unable to load vCards from "
+                    + inputFileName2 + ":\n{0}",
+                    ex.Message);
+                if (ex.InnerException != null) {
+                    error += ex.InnerException.Message + "\n";
+                    if (ex.InnerException.InnerException != null)
+                        error += ex.InnerException.InnerException.Message;
+                }
+                System.Diagnostics.Debug.Write(ex);
+                Utils.excMsg(error, ex);
+                return;
+            }
+            Console.WriteLine("Read " + inputFileName2);
+            Console.WriteLine("There are " + vCards2.Count + " vCards");
+
+            // Loop over the second file
+            foreach (VCard vCard2 in vCards2) {
+                foreach (VCard vCard in vCards) {
+                    if (vCard2.Name.FormattedName
+                        .Equals(vCard.Name.FormattedName)) {
+                        // The names match
+                        if (vCard.Photo.Value != null &&
+                            vCard.Photo.ValueLocation == ValLocValue.Binary) {
+                            // There is an image in the first one
+                            byte[] imageBytes = vCard.Photo.GetImageBytes();
+                            vCard2.Photo.SetImageBytes(imageBytes);
+                            vCard2.Photo.ImageType = vCard.Photo.ImageType;
+                        }
+                        break;
+                    }
+                } // foreach vCard1
+            }  // foreach vCard2
+
+            string name = Path.GetFileName(inputFileName2);
+            string prefix = Path.GetFileNameWithoutExtension(name);
+            string ext = Path.GetExtension(inputFileName2);
+            string outputFileName2 = Path.Combine(outputDirName2,
+                name + ".Combined" + ext);
+
+            using (var sw = new StreamWriter(outputFileName2, false,
+                PDIParser.DefaultEncoding)) {
+                vCards.WriteToStream(sw);
+            }
+            Console.WriteLine("Wrote " + outputFileName2);
+        }
+
+        /// <summary>
+        /// Finds images in inputFileName and saves them as PNG files in
+        /// outputDirName2.
+        /// </summary>
+        private static void saveImages() {
+            VCardCollection vCards;
+            try {
+                vCards = VCardParser.ParseFromFile(inputFileName);
+            } catch (Exception ex) {
+                string error = String.Format("Unable to load vCards from "
+                    + inputFileName + ":\n{0}",
+                      ex.Message);
                 if (ex.InnerException != null) {
                     error += ex.InnerException.Message + "\n";
                     if (ex.InnerException.InnerException != null)
@@ -34,6 +128,7 @@ namespace VCardExtractPhotos {
             bool hasImage;
             string imageType;
             string info = "";
+            int nPhotos = 0;
             foreach (VCard vCard in vCards) {
                 hasImage = false;
                 imageFileName = null;
@@ -54,9 +149,14 @@ namespace VCardExtractPhotos {
                 info += LF;
                 Console.Write(info);
                 if (hasImage) {
+                    nPhotos++;
                     saveImage(vCard);
                 }
             }
+            Console.WriteLine("There are " + vCards.Count + " vCards in "
+                + inputFileName);
+            Console.WriteLine("Wrote " + nPhotos + " photos to "
+                + outputDirName);
         }
 
         static void saveImage(VCard vCard) {
@@ -83,7 +183,8 @@ namespace VCardExtractPhotos {
         }
 
         static void Main(string[] args) {
-            saveImages();
+            //saveImages();
+            transferImages();
         }
     }
 }
